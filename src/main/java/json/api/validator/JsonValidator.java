55aa118986ba7d657e575a279ec233ed376d8811
AvasBrahma <avasbrahma@gmail.com>
2025-08-07 21:28:34 +0530

    added json validator method

--- src/main/java/json/api/validator/JsonValidator.java
+++ src/main/java/json/api/validator/JsonValidator.java
@@ -1,22 +1,56 @@
 package json.api.validator;
 import org.json.JSONObject;
+import com.jayway.jsonpath.JsonPath;
+import org.testng.Assert;
 
 public class JsonValidator {
 	
+	
+	//JSON Path value validation using JSON Objects
 	public static void validateJsonResponse(String responseBody, String jsonPath, String expectedValue) {
-        JSONObject json = new JSONObject(responseBody);
+		try {
+	        JSONObject json = new JSONObject(responseBody);
+	        String[] pathParts = jsonPath.split("\\.");
+	        Object current = json;
 
-            if(json.has(jsonPath)) {
-               String actualValue=json.get(jsonPath).toString();
-               if(actualValue==expectedValue) {
-            	   System.out.println("Validation Passed:  Expected : "+expectedValue+" and Actual value : "+actualValue);
-               }else {
-                 System.out.println("Validation Fail, Mismatch Expected : "+expectedValue+" and Actual value : "+actualValue);
-               }
-            }else {
-            	System.out.println("JSON does not contain path: " + jsonPath);
-            }
+	        for (String key : pathParts) {
+	            if (current instanceof JSONObject && ((JSONObject) current).has(key)) {
+	                current = ((JSONObject) current).get(key);
+	            } else {
+	                System.out.println("JSON does not contain path: " + jsonPath);
+	                return;
+	            }
+	        }
+
+	        String actualValue = current.toString();
+	        if (actualValue.equals(expectedValue)) {
+	            System.out.println("Validation Passed: Expected: " + expectedValue + ", Actual: " + actualValue);
+	        } else {
+	            System.out.println("Validation Failed: Mismatch. Expected: " + expectedValue + ", Actual: " + actualValue);
+	        }
+	    } catch (Exception e) {
+	        System.out.println("Error while validating JSON: " + e.getMessage());
+	    }
+
            
 	}
+	
+
+    // json path validation using
+	public static void validateJsonResponseUsingJSONPath(String responseBody, String jsonPath, String expectedValue) {
+	    try {
+	        String actualValue = JsonPath.read(responseBody, "$." + jsonPath).toString();
+
+	        if (actualValue.equals(expectedValue)) {
+	            System.out.println("Validation Passed: Expected: " + expectedValue + ", Actual: " + actualValue);
+	        } else {
+	            //System.out.println("Validation Failed: Mismatch. Expected: " + expectedValue + ", Actual: " + actualValue);
+	            Assert.assertEquals(actualValue, expectedValue, "Validation Failed");
+	        }
+	    } catch (Exception e) {
+	        System.out.println("Error extracting value with path: " + jsonPath + " => " + e.getMessage());
+	    }
+	}
+
 
 }
